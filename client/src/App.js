76db121d7db572f2aeed36d1f2893d387fd1a37f
Esther Cho <esthercho@Esthers-MacBook-Air.local>
2019-11-06 19:08:20 -0700

    added React code on front end for skeleton Nav/Login/Register Modals

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+// import PostModal from './components/PostModal';
+import { Container } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/authActions';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className='App'>
+          <AppNavbar />
+          <Container>
+            {/* <ItemModal /> */}
+            {/* <ShoppingList /> */}
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
