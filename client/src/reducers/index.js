50f88a6335ddea4d14ee25df14686bfe0dd1d311
Paul Graven <graven.paul@gmail.com>
2019-11-12 22:18:03 -0700

    App.js to include PostModal

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,10 @@
-import { combineReducers } from 'redux';
-import errorReducer from './errorReducer';
-import authReducer from './authReducer';
+import { combineReducers } from "redux";
+import errorReducer from "./errorReducer";
+import authReducer from "./authReducer";
 // import postReducer from './postReducer';
 
 export default combineReducers({
-auth: authReducer,
-error: errorReducer,
-// item: postReducer
+  auth: authReducer,
+  error: errorReducer
+  // item: postReducer
 });
