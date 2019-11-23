5058a4c10640bc443d6e77d7cccabf1a0a219f01
Peace Akpai <peaceawo@gmail.com>
2019-11-22 20:05:02 -0700

    pull request

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -14,76 +14,73 @@ import {
   CardImg,
   Button
 } from "reactstrap";
-
 class GetPosts extends Component {
   static propTypes = {
     auth: PropTypes.object.isRequired,
     isAuthenticated: PropTypes.bool
   };
-
   state = {
     posts: []
   };
-
   componentDidMount() {
-    this.getPostsbyUser();
+    this.getPosts();
   }
-
-  getPostsbyUser = () => {
+  getPosts = () => {
     return axios
-      .get("/api/posts/user")
-      .then(res =>
+      .get("/api/posts")
+      .then(res => {
         this.setState({
           posts: res.data
-        })
-      )
+        });
+      })
       .catch(err => console.log(err));
   };
-
   render() {
+    const { isAuthenticated, user } = this.props.auth;
     return (
       <Container>
         {this.props.isAuthenticated ? (
           <div>
             {this.state.posts.length ? (
               <Row>
-                {this.state.posts.map(post => (
-                  // <ul className="posts">
-
-                  <Card className="card" width="10px" key={post._id}>
-                    {/* user={post.user} */}
-                    {/* // authors={post.authors.join(", ")} */}
-                    <CardHeader>
-                      {post.city}, {post.country}
-                    </CardHeader>
-                    <CardHeader>{post.continent}</CardHeader>
-                    <CardImg
-                      className="img-responsive"
-                      src={post.photo}
-                      alt="photo"
-                    ></CardImg>
-                    <CardText className="cardText">{post.description}</CardText>
-                    <Button className="likebtn"></Button>
-                  </Card>
-                  // </ul>
-                ))}
+                {this.state.posts.map(post => {
+                  if (post.user === user._id) {
+                    return (
+                      <Card className="card" width="10px">
+                        <CardHeader className="CardHeader">
+                          {post.city}, {post.country}
+                        </CardHeader>
+                        {/* <CardHeader>{post.continent}</CardHeader> */}
+                        <CardImg
+                          className="img-responsive"
+                          src={post.photo}
+                          alt="photo"
+                        ></CardImg>
+                        <CardText className="cardText">
+                          {post.description}
+                        </CardText>
+                        <Button className="likebtn">
+                          <i class="fas fa-plane"></i>
+                        </Button>
+                      </Card>
+                    );
+                  }
+                })}
               </Row>
             ) : (
               <h2 className="text-center">No posts have been made</h2>
             )}
           </div>
         ) : (
-          <h4 className="mb-3 ml-4">Please log in to manage posts</h4>
+          <h4 className="mb-3 ml-4"> </h4>
         )}
       </Container>
     );
   }
 }
-
 const mapStateToProps = state => ({
   post: state.post,
   auth: state.auth,
   isAuthenticated: state.auth.isAuthenticated
 });
-
 export default connect(mapStateToProps, { getPost })(GetPosts);
